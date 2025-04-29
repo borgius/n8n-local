119c647e8a70964fd0d1b0977ef00808840872b8
Viktor Borg <868919+borgius@users.noreply.github.com>
2025-04-29 11:25:28 -0400

    feat: update Dockerfile and init script for improved package management and initialization process

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM n8nio/n8n:latest
-USER root
-RUN set -x; apk update && apk add sudo curl nano jq && \
-    echo "node ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/node && \
-    chmod 0440 /etc/sudoers.d/node
-USER node  
-
-COPY n8n /home/node/n8n 
-RUN n8n/scripts/init.sh nodeModules
--- n8n/scripts/init.sh
+++ n8n/scripts/init.sh
@@ -1,71 +1,80 @@
 #!/bin/bash
-has() { type "$1" &> /dev/null; }
+has() { type "$1" &>/dev/null; }
++() {
+  echo "+ $@"
+  "$@"
+}
 
-init(){
-  has sudo || {
-    sudo apk update 
-    sudo apk add sudo curl
+gitInit() {
+  cd /home/node
+  test -f .gitignore || {
+    echo "Creating .gitignore"
+    printf 'node_modules\n.pnpm-store\n.npm\n.cache'>.gitignore
+    set -x
+    git init
+    git config --global user.name "n8n"
+    git config --global user.email "n8n@localhost"
+    git add .
+    git commit -m "Initial commit"
+    set +x
   }
 }
 
-fixPermissions(){
+fixPermissions() {
   echo "Fixing permissions"
   set -x
+  mkdir -p /home/node/.n8n/nodes
   sudo chown -R node:node /home/node/.n8n/nodes
   set +x
 }
 
-n8nImport(){
+n8nImport() {
   if [ -d "/backup/credentials" ] && [ -n "$(ls -A /backup/credentials 2>/dev/null)" ]; then
     echo "Found credentials to import"
-    n8n import:credentials --separate --input=/backup/credentials
+    + n8n import:credentials --separate --input=/backup/credentials
   else
     echo "No credentials found in /backup/credentials, skipping credentials import"
   fi
 
   if [ -d "/backup/workflows" ] && [ -n "$(ls -A /backup/workflows 2>/dev/null)" ]; then
     echo "Found workflows to import"
-    n8n import:workflow --separate --input=/backup/workflows
+    + n8n import:workflow --separate --input=/backup/workflows
   else
     echo "No workflows found in /backup/workflows, skipping workflows import"
   fi
 }
 
-nodeModules(){
-  set -x
-  sudo chown -R node:node /home/node/n8n
-  cd /home/node/n8n
-  npm install
-  local n8nModules=/usr/local/lib/node_modules/n8n/node_modules
-  find node_modules -mindepth 1 -maxdepth 1 -type d -exec sudo mv -v {} $n8nModules/ \;
-  set +x
-}
-
-mountModules() {
-  cd /home/node
+extraPackages() {
+  + cd /home/node/packages
   local n8nModules=/usr/local/lib/node_modules/n8n/node_modules
   echo "Create a links to local packages"
-  ls -1 packages | while read pkg; do
+  find . -mindepth 1 -maxdepth 1 -type d -not -name "node_modules" | xargs -n1 basename | while read pkg; do
     if [ ! -L "$n8nModules/$pkg" ] && [ ! -d "$n8nModules/$pkg" ]; then
       sudo ln -sv "/home/node/packages/$pkg" "$n8nModules/$pkg"
     else
       echo "Link for $pkg already exists, skipping"
     fi
   done
+  mkdir -p node_modules
+  # Fix permissions for node_modules
+  sudo chown -R node:node node_modules
+  + npm install
+  find node_modules -mindepth 1 -maxdepth 1 -type d -exec sudo mv -v {} $n8nModules/ \;
+  + cd /home/node
 }
 
 communityNodes() {
-  mkdir -p /home/node/.n8n/nodes
-  npm install
+  + mkdir -p /home/node/.n8n/nodes
+  if [ -d "/home/node/_communityNodes" ]; then
+    cp -rfv /home/node/_communityNodes/* /home/node/.n8n/nodes/
+  fi
+  + npm install
 }
 
 customNodes() {
-  mkdir -p /home/node/.n8n/custom
-  npm install
-}
-
-installMcpServers() {
-  has pm2 || sudo npm install @go-task/cli mcp-proxy pm2@latest -g
+  mkdir -p /home/node/.n8n/custom/node_modules
+  + chown -R node:node node_modules
+  + npm install
 }
 
 startMcpServers() {
@@ -84,5 +93,5 @@ entrypoint() {
 }
 
 for cmd in $@; do
-  has $cmd && $cmd
+  has $cmd && + $cmd
 done
